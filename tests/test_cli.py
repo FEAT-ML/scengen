180c736a3094c4d11013e8f26b12f795de6d3236
Felix Nitsch <felix.nitsch@dlr.de>
2023-09-19 16:16:27 +0200

    fixed: pytests for resolve_relative_paths

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -6,15 +6,13 @@ from scengen.cli import resolve_relative_paths
 class Test:
     @staticmethod
     def test_resolve_relative_paths__resolve_absolute_path():
-        args = {"path": Path(r"D:\my\path.file")}
-        assert args["path"].is_absolute()
+        args = {"path": Path(r"Y:\my\path.file")}
         resolved_args = resolve_relative_paths(args)
         assert resolved_args["path"].is_absolute()
 
     @staticmethod
     def test_resolve_relative_paths__resolve_relative_path():
         args = {"path": Path(r"\my\path.file")}
-        assert not args["path"].is_absolute()
         resolved_args = resolve_relative_paths(args)
         assert resolved_args["path"].is_absolute()
 
