1c729641c678f7b2f18de1edb09d50bcc3845ff0
Christoph Schimeczek <Christoph.Schimeczek@dlr.de>
2024-08-01 11:39:25 +0200

    Fix wrong path in evaluator.py

--- scengen/evaluator.py
+++ scengen/evaluator.py
@@ -9,7 +9,7 @@ import pandas as pd
 from amirispy.source.cli import RunOptions
 
 
-NAME_ENERGY_EXCHANGE = "EnergyExchange"
+NAME_ENERGY_EXCHANGE = "DayAheadMarketSingleZone"
 NAME_ELECTRICITY_PRICE_COLUMN = "ElectricityPriceInEURperMWH"  # noqa
 SCARCITY_PRICE = 3000
 THRESHOLD_SHARE_SCARCITY_HOURS = 0.10
@@ -25,7 +25,7 @@ def evaluate_scenario(options: dict) -> bool:
 
 def scarcity_occurrence(options: dict) -> bool:
     """Returns True if occurrences of SCARCITY_PRICE is within THRESHOLD_SHARE_SCARCITY_HOURS"""
-    path_to_exchange = Path(options[RunOptions.OUTPUT], options["scenario_name"], f"{NAME_ENERGY_EXCHANGE}.csv")
+    path_to_exchange = Path(options[RunOptions.OUTPUT], f"{NAME_ENERGY_EXCHANGE}.csv")
     energy_exchange = pd.read_csv(path_to_exchange, sep=";")
     scarcity_hours = (energy_exchange[NAME_ELECTRICITY_PRICE_COLUMN] >= SCARCITY_PRICE).sum()
     n_of_tolerated_hours = round(len(energy_exchange) * THRESHOLD_SHARE_SCARCITY_HOURS)
